680bdc0702ba0c0d606127abdc1013dfed83c16a
Russell Hay <me@russellhay.com>
2016-07-17 08:50:38 -0700

    step 3

--- index.js
+++ index.js
@@ -2,19 +2,12 @@ var pixel = require('node-pixel');
 var five = require('johnny-five');
 var forecast = require('./lib/forecast');
 var temperature = require('./lib/temperature');
+var breath = require('./lib/breath');
 
 var board = new five.Board();
 var strip = null;
-var status = false;
 var mainColor = '#000000';
 var numberOfHours = 4;
-
-forecast.fetch(function (err, weather) {
-  if (err) return console.dir(err);
-  var avgTemperature = temperature.average(weather.hourly, numberOfHours);
-  mainColor = temperature.mapToColor(avgTemperature);
-  console.log(mainColor);
-});
 
 board.on('ready', function () {
   strip = new pixel.Strip({
@@ -24,14 +17,16 @@ board.on('ready', function () {
   });
 
   strip.on('ready', function () {
-    setInterval(function () {
-      if (status) {
-        strip.color(mainColor);
-      } else {
-        strip.color('#000000');
-      }
-      strip.show();
-      status = !status;
-    }, 500); // 1Hz 50/50 duty cycle
+    forecast.fetch(function (err, weather) {
+      if (err) return console.dir(err);
+      var avgTemperature = temperature.average(weather.hourly, numberOfHours);
+      mainColor = temperature.mapToColor(avgTemperature);
+
+      setInterval(function () {
+        var currentColor = breath.single(mainColor);
+        strip.color(currentColor.hex());
+        strip.show();
+      }, 100); // 10 Hz
+    });
   });
 });
